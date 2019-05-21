a8720cd31c9529c6a943317bf3b5b0c8af60ae79
YauheniRusinchuk <ruevgal@gmail.com>
2019-05-21 14:09:58 +0300

    add login post request

--- src/apps/login/views.py
+++ src/apps/login/views.py
@@ -1,7 +1,18 @@
 from django.views import View
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
 
 class Login(View):
 
     def get(self, request, *args, **kwargs):
         return render(request, 'login/login.html', {})
+
+    def post(self, request, *args, **kwargs):
+        email       = request.POST.get('email')
+        password    = request.POST.get('password')
+        user = authenticate(request, email=email, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home:home_page')
+        else:
+            return redirect('login:login_page')
