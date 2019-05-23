d6456040a9a6400dfbf354fddccdf6d9013a5b1b
YauheniRusinchuk <ruevgal@gmail.com>
2019-05-23 15:56:37 +0300

    add post method new post

--- src/apps/home/views.py
+++ src/apps/home/views.py
@@ -9,5 +9,11 @@ class Home(View):
     def get(self, request, *args, **kwargs):
         if not request.user.is_authenticated:
             return redirect('login:login_page')
-        twits = Twit.objects.filter(profile__in=request.user.profile.followers.all())    
+        twits = Twit.objects.filter(profile__in=request.user.profile.followers.all())
         return render(request, 'home/index.html', {'twits': twits})
+
+
+    def post(self, request, *args, **kwargs):
+        twit = Twit(profile=request.user.profile, text=request.POST.get('text'))
+        twit.save()
+        return redirect('home:home_page')
